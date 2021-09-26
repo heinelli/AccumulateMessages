d1db1733299ac5623367fbfac42cd9e0c027e324
Elliot Heiner <elliotheiner1997@gmail.com>
2021-09-26 14:21:42 -0600

    Add header, change module name, add option explicit/strinct.

---
+++ AccumulateMessages/AccumulateMessages.vb
@@ -0,0 +1,16 @@
+﻿'Elliot Heiner
+'RCET 0265
+'Fall 2021
+'Accumulate Messages Function
+'https://github.com/heinelli/AccumulateMessages.git
+
+Option Explicit On
+Option Strict On
+
+Module AccumulateMessages
+
+    Sub Main()
+
+    End Sub
+
+End Module
