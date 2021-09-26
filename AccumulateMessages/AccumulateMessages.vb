668c2ad32d333b3f953d3b40b81b47fa98de4cd0
Elliot Heiner <elliotheiner1997@gmail.com>
2021-09-26 15:36:12 -0600

    Add third function variable and option to clear stored messages. Add option to compare text.

--- AccumulateMessages/AccumulateMessages.vb
+++ AccumulateMessages/AccumulateMessages.vb
@@ -6,24 +6,43 @@
 
 Option Explicit On
 Option Strict On
+Option Compare Text
 
 Module AccumulateMessages
 
     Sub Main()
         Dim newMessage As String
         Dim clear As Boolean = False
+        Dim storage As String
+        Dim endLoop As Boolean
 
-        Console.WriteLine("Type a message here.")
-        newMessage = Console.ReadLine()
-        UserMessages(newMessage, clear)
-        Console.WriteLine(UserMessages(newMessage, clear))
-        Console.ReadLine()
+
+
+        Do While endLoop = False
+
+            If newMessage = "q" Then
+                endLoop = True
+            Else
+                Console.WriteLine("Type a message here. Press C to clear stored messages. Press Q to terminate program")
+                newMessage = Console.ReadLine()
+                UserMessages(newMessage, clear, storage)
+                Console.WriteLine(UserMessages(newMessage, clear, storage))
+                storage = UserMessages(newMessage, clear, storage)
+            End If
+        Loop
+
 
     End Sub
 
-    Function UserMessages(ByVal newMessage As String, ByVal clear As Boolean) As String
-        Dim returnMessage As String = newMessage
-        Return returnMessage
+    Function UserMessages(ByVal newMessage As String, ByVal clear As Boolean, ByVal storage As String) As String
+
+        Dim functionStorage As String = newMessage + vbNewLine + storage
+
+        If newMessage = "C" Then
+            functionStorage = ""
+        End If
+
+        Return functionStorage
     End Function
 
 
